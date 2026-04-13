9467065f9597f472956d09db26d7c8cd4ed2c631
Yugal Raj Vimal <yugalrajvimal@Yugals-MacBook-Air.local>
2026-04-13 16:23:49 +0530

    --

--- Schema/deals.schema.js
+++ Schema/deals.schema.js
@@ -1,57 +1,83 @@
 import mongoose from 'mongoose';
 
-const dealSchema = new mongoose.Schema({
-  dealType: {
-    type: String,
-    required: true,
-    enum: ['Service', 'Parts'],
+const selectedVehicleSchema = new mongoose.Schema(
+  {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    model: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    year: {
+      type: String,
+      required: true,
+      trim: true,
+    },
   },
-  serviceId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Services",
-    required: function() { return this.dealType === 'Service'; }
+  { _id: false }
+);
+
+const dealSchema = new mongoose.Schema(
+  {
+    dealType: {
+      type: String,
+      required: true,
+      enum: ["Service", "Parts"],
+    },
+    serviceId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Services",
+      required: function () {
+        return this.dealType === "Service";
+      },
+    },
+    partName: {
+      type: String,
+      required: function () {
+        return this.dealType === "Parts";
+      },
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+      default: "",
+    },
+    selectedVehicle: {
+      type: selectedVehicleSchema,
+    },
+    discountedPrice: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    offerEndsOnDate: {
+      type: Date,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "BusinessProfile",
+      required: true,
+    },
+    imagePath: {
+      type: String,
+      required: false,
+      trim: true,
+      default: null,
+    },
   },
-  partName: {
-    type: String,
-    required: function() { return this.dealType === 'Parts'; },
-    trim: true,
-  },
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-    default: "",
-  },
-  vehicleTypeId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "VehicleType",
-    required: false
-  },
-  discountedPrice: {
-    type: Number,
-    required: true,
-    min: 0
-  },
-  offerEndsOnDate: {
-    type: Date,
-    required: true
-  },
-  province: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  dealEnabled: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-  createdBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "BusinessProfile",
-    required: true
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 const DealModel = mongoose.model("Deal", dealSchema);
 export default DealModel;
