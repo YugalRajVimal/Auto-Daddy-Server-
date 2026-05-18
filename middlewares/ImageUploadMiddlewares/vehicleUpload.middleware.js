ce714c0bb49ad21c5c74a31cdf5152614c8ea9ff
Yugal Raj Vimal <yugalrajvimal@Yugals-MacBook-Air.local>
2026-05-18 12:39:14 +0530

    --

--- middlewares/ImageUploadMiddlewares/vehicleUpload.middleware.js
+++ middlewares/ImageUploadMiddlewares/vehicleUpload.middleware.js
@@ -4,7 +4,13 @@ import { upload } from "./fileUpload.middleware.js";
 // Updated to match the vehicle document field names in user.schema.js (VehicleDocumentSchema)
 export const vehicleUploadMiddleware = (req, res, next) => {
   upload.fields([
+    { name: "carOwnershipCertificate", maxCount: 1 },
+    { name: "insuranceCertificate", maxCount: 1 },
+    { name: "carImage", maxCount: 1 },
+    { name: "drivingLicenseFront", maxCount: 1 },
+    { name: "drivingLicenseBack", maxCount: 1 },
     { name: "vehicleImage", maxCount: 1 },
+
   ])(req, res, (err) => {
     if (err instanceof multer.MulterError) {
       return res.status(400).json({
