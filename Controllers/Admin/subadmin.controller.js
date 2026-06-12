6023edd4533516d268014e514a09be4841e87479
Yugal Raj Vimal <yugalrajvimal@192.168.1.2>
2026-06-13 03:49:02 +0530

    --

--- Controllers/Admin/subadmin.controller.js
+++ Controllers/Admin/subadmin.controller.js
@@ -5,7 +5,7 @@ import bcrypt from "bcryptjs";
 import { SubAdminActivity } from "../../Schema/subadmin-activity.schema.js";
 import ExpiredTokenModel from "../../Schema/expired-token.schema.js";
 import { Admin } from "../../Schema/admin.schema.js";
-import { SubAdmin } from "../../Schema/Subadmin.schema.js";
+import { SubAdmin } from "../../Schema/subadmin.schema.js";
 
 // Helper: log activity
 async function logActivity({ performedBy, performedByRole, performedByName, action, module, description, targetSubAdmin, ipAddress, metadata }) {
